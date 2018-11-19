00750c021c5863205aea66a51d2ca01a4dd013d1
Anabelle Baynard <baynardanabelle@gmail.com>
2018-11-19 17:59:40 +0100

    quete parser son json

--- WeatherParse.java
+++ WeatherParse.java
@@ -23,17 +23,35 @@ public class WeatherParse {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        
-        // TODO parser le fichier
 
+        // TODO parser le fichier
+        JSONParser parser = new JSONParser();
+        Object jsonParsed = null;
+        try {
+          jsonParsed = parser.parse(jsonFile);
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
         // TODO récupérer la racine du document
+        JSONObject root = (JSONObject) jsonParsed;
 
         // TODO afficher la valeur de l'attribut "name" de la racine
-
+        String name = (String) root.get("name");
+        System.out.println("City name : "+ name);
         // TODO afficher les valeurs des attributs "lat" et "lon" de l'élément "coord" contenu dans la racine
-
+        JSONObject coord = (JSONObject) root.get ("coord");
+        double lat = (double) coord.get("lat");
+        double lon = (double) coord.get("lon");
+        System.out.println("City latitude : "+lat);
+        System.out.println("City longitude : "+lon);
         // TODO parcourir tous les éléments de "weather" et afficher le contenu de "main"
-
+        JSONArray weather = (JSONArray) root.get("weather");
+        for (int i=0;i<weather.size();i++){
+        //  JSONArray weather = (JSONArray) root.get("weather");
+          JSONObject weatherI = (JSONObject) weather.get(i);
+          String main = (String) weatherI.get("main");
+          System.out.println("Weather : "+main);
+        }
         /*
             Résultat attendu :
             * City name: London
